86fa84e7485b4a8abcd12defd28e9ad7f8e6bc95
WANG XIAOLI <71541515+lily983@users.noreply.github.com>
2023-06-14 22:11:27 +0800

    add param notation in test script

--- test/testMethodsR3.m
+++ test/testMethodsR3.m
@@ -1,9 +1,13 @@
 clc; clear; close all;
 add_path()
-%% Generate 500 random samples for spheres with size range (1, 10)
-resultsSphere = getResultsR3(1, 10, 'sphere');
+
+sizeScale = 1;
+sampleNumber = 50;
+
+%% Generate 50 random samples for spheres with size range (1, 10)
+resultsSphere = getResultsR3(sizeScale, sampleNumber, 'sphere');
 visualizeResults(resultsSphere, 'sphere');
 
-%% Generate 500 random samples for ellipsoid with size range (1, 10)
-resultsEllip = getResultsR3(1, 10, 'ellip');
+%% Generate 50 random samples for ellipsoid with size range (1, 10)
+resultsEllip = getResultsR3(sizeScale, sampleNumber, 'ellip');
 visualizeResults(resultsEllip, 'ellip');
