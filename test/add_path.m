185d38ccc8ceabaa8a7ebbf478cc52f32f385906
WANG XIAOLI <71541515+lily983@users.noreply.github.com>
2023-06-15 10:48:57 +0800

    change add_path

--- test/add_path.m
+++ test/add_path.m
@@ -1,8 +1,6 @@
 addpath ../src/
-addpath ../src/pkf/
 addpath ../src/util/
-addpath ../src/matlab
-addpath ../src/GMM
-addpath ../src/util/clipped_integration
-addpath ../src/R3
-addpath ../src/SE3
+addpath ../src/geometry
+addpath ../src/positionErrorPCD
+addpath ../src/poseErrorPCD
+addpath ../src/visualizationUtil
