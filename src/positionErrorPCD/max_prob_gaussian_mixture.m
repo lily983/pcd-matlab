f2a000ac0447c19a563ab462bf5e776fbc6a3a79
WANG XIAOLI <71541515+lily983@users.noreply.github.com>
2023-08-24 15:09:15 +0800

    add 3SG, 4SG, 5SG for GMM

--- src/positionErrorPCD/max_prob_gaussian_mixture.m
+++ src/positionErrorPCD/max_prob_gaussian_mixture.m
@@ -1,6 +1,6 @@
-function prob = max_prob_gaussian_mixture(s1, s2, mx, Sigmax)
+function prob = max_prob_gaussian_mixture(s1, s2, mx, Sigmax, method1, method2)
 % Check dimension 
-dimension = size(s1.a, 2);
+dimension = size(Sigmax, 2);
 
 %Check if s1 and s2 are superquadric 
 s1objectType = getObjectType(s1);
@@ -8,14 +8,25 @@ s2objectType = getObjectType(s2);
 
 if strcmp(s1objectType, 'superquadric')==1 && strcmp(s2objectType, 'superquadric')==1
     prob = NaN;
-    weightedNorm = NaN;
-    error('Input objects are not sphere or ellip, unable to use PCD-SG');
+    error('Input objects are not sphere or ellip, unable to use PCD-GMM');
 end
 
-[mf, Sigmaf] = get_bounding_ellip(s1, s2);
+%choose which bounding ellipsoid methods to use
+switch method1
+    case 'fast-computation'
+        [mf, Sigmaf] = get_bounding_ellip(s1, s2);
+    case 'accurate-result'
+        [mf, Sigmaf] = get_bounding_ellip_fixed_point(s1, s2);
+end
 
-[a1,a2,b1,b2] = get_gmm_param(Sigmaf, 10, 3);
+%choose which gmm to use(2 SG or 3SG or 4SG or 5SG)
+[a, b] = get_gmm_param(Sigmaf, method2);
 
-prob = a1 * Gaussian_integration(mf, Sigmaf/b1, mx, Sigmax) / b1^(dimension/2) ...
-    + a2 * Gaussian_integration(mf, Sigmaf/b2, mx, Sigmax) / b2^(dimension/2) ;
+% compute probability of collision 
+prob=0;
+
+for i=1:size(a,2)
+    prob = prob + a(i)*Gaussian_integration(mf, Sigmaf/b(i), mx, Sigmax) / b(i)^(dimension/2);
+end
+
 end
