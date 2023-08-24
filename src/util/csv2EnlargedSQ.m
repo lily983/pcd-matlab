cd2719b31508252a3a5a3294ee9dcda207e8d961
WANG XIAOLI <71541515+lily983@users.noreply.github.com>
2023-08-24 20:33:18 +0800

    add file to change csv to enlarged bounding SQ

---
+++ src/util/csv2EnlargedSQ.m
@@ -0,0 +1,39 @@
+function enlarged_SQ_csv = csv2EnlargedSQ(csv, distribution, CL)
+%CL: confidence level('95', '99')
+
+if nargin <3
+    CL='95';
+end
+
+%axis-angle
+SQaxis = csv(1, 9:11);
+SQaxis = SQaxis./norm(axis,2);
+SQangle = csv(1, 12);
+quat = axang2quat([SQaxis, SQangle]);
+
+% Build original SQ
+sq = SuperQuadrics({csv(1, 1:3), [csv(4), csv(5)], [0,0],...
+    [csv(6), csv(7), csv(8)]', [quat(1), quat(2), quat(3), quat(4)], [10, 10]});
+
+% enlarged SQ
+Mu = [eye(3), sq.tc; 0, 0, 0, 1];
+Sigma = zeros(6,6);
+Sigma(4:6, 4:6) = positionErrorDistribution('ellipsoid', distribution);
+enlarged_SQ = enlarged_bounding_superquadrics(sq, Mu, Sigma, CL);
+
+enlarged_SQ_csv(1, 1:3) = enlarged_SQ.a;
+enlarged_SQ_csv(1, 4:5) = enlarged_SQ.eps;
+enlarged_SQ_csv(1, 6:8) = enlarged_SQ.tc';
+
+% quat
+enlarged_SQ_axang = quat2axang(enlarged_SQ.q);
+enlarged_SQ_csv(1, 9:11) = enlarged_SQ_axang(1,1:3);
+enlarged_SQ_csv(1, 12) = enlarged_SQ_axang(1,4);
+
+% visualization
+figure; hold on; 
+axis equal;
+sq.PlotShape('r', 0.6);
+enlarged_SQ.PlotShape('b', 0.3);
+
+end
