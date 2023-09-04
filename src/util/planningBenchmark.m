83ff1e96f2a286100d4da7abb45c42389f5cc2bf
WANG XIAOLI <71541515+lily983@users.noreply.github.com>
2023-09-04 21:49:14 +0800

    update environment in planning benchmark

--- src/util/planningBenchmark.m
+++ src/util/planningBenchmark.m
@@ -7,11 +7,17 @@ PCDEB95_narrow_small_path = base_path + "PCDEB95_narrow_small.csv";
 PCDEB95_sparse_large_path = base_path + "PCDEB95_sparse_large.csv";
 PCDEB95_sparse_small_path = base_path + "PCDEB95_sparse_small.csv";
 
+PCDEB95_clamp_large_path = base_path + "PCDEB95_clamp_large.csv";
+PCDEB95_clamp_small_path = base_path + "PCDEB95_clamp_small.csv";
+
 PCDGMM_narrow_large_path = base_path + "PCDGMM_narrow_large.csv";
 PCDGMM_narrow_small_path = base_path + "PCDGMM_narrow_small.csv";
 
 PCDGMM_sparse_large_path = base_path + "PCDGMM_sparse_large.csv";
 PCDGMM_sparse_small_path = base_path + "PCDGMM_sparse_small.csv";
+
+PCDGMM_clamp_large_path = base_path + "PCDGMM_clamp_large.csv";
+PCDGMM_clamp_small_path = base_path + "PCDGMM_clamp_small.csv";
 %%
 PCDEB95_narrow_large = readtable(PCDEB95_narrow_large_path);
 PCDEB95_narrow_small = readtable(PCDEB95_narrow_small_path);
@@ -19,42 +25,62 @@ PCDEB95_narrow_small = readtable(PCDEB95_narrow_small_path);
 PCDEB95_sparse_large = readtable(PCDEB95_sparse_large_path);
 PCDEB95_sparse_small = readtable(PCDEB95_sparse_small_path);
 
+PCDEB95_clamp_large = readtable(PCDEB95_clamp_large_path);
+PCDEB95_clamp_small = readtable(PCDEB95_clamp_small_path);
+
 PCDGMM_narrow_large = readtable(PCDGMM_narrow_large_path);
 PCDGMM_narrow_small = readtable(PCDGMM_narrow_small_path);
 
 PCDGMM_sparse_large = readtable(PCDGMM_sparse_large_path);
 PCDGMM_sparse_small = readtable(PCDGMM_sparse_small_path);
+
+PCDGMM_clamp_large = readtable(PCDGMM_clamp_large_path);
+PCDGMM_clamp_small = readtable(PCDGMM_clamp_small_path);
 %%
-PCDEB95_narrow_large_path_length = table2array(PCDEB95_narrow_large(1:size(PCDEB95_narrow_large,1)-2,10));
-PCDEB95_narrow_small_path_length = table2array(PCDEB95_narrow_small(1:size(PCDEB95_narrow_small,1)-2,10));
+% PCDEB95_narrow_large_path_length = table2array(PCDEB95_narrow_large(1:size(PCDEB95_narrow_large,1)-2,10));
+% PCDEB95_narrow_small_path_length = table2array(PCDEB95_narrow_small(1:size(PCDEB95_narrow_small,1)-2,10));
+% 
+% PCDEB95_sparse_large_path_length = table2array(PCDEB95_sparse_large(1:size(PCDEB95_sparse_large,1)-2,10));
+% PCDEB95_sparse_small_path_length = table2array(PCDEB95_sparse_small(1:size(PCDEB95_sparse_small,1)-2,10));
 
-PCDEB95_sparse_large_path_length = table2array(PCDEB95_sparse_large(1:size(PCDEB95_sparse_large,1)-2,10));
-PCDEB95_sparse_small_path_length = table2array(PCDEB95_sparse_small(1:size(PCDEB95_sparse_small,1)-2,10));
+PCDEB95_clamp_large_path_length = table2array(PCDEB95_clamp_large(1:size(PCDEB95_clamp_large,1)-2,10));
+PCDEB95_clamp_small_path_length = table2array(PCDEB95_clamp_small(1:size(PCDEB95_clamp_small,1)-2,10));
 
-PCDGMM_narrow_large_path_length = table2array(PCDGMM_narrow_large(1:size(PCDGMM_narrow_large,1)-2,10));
-PCDGMM_narrow_small_path_length = table2array(PCDGMM_narrow_small(1:size(PCDGMM_narrow_small,1)-2,10));
+% PCDGMM_narrow_large_path_length = table2array(PCDGMM_narrow_large(1:size(PCDGMM_narrow_large,1)-2,10));
+% PCDGMM_narrow_small_path_length = table2array(PCDGMM_narrow_small(1:size(PCDGMM_narrow_small,1)-2,10));
+% 
+% PCDGMM_sparse_large_path_length = table2array(PCDGMM_sparse_large(1:size(PCDGMM_sparse_large,1)-2,10));
+% PCDGMM_sparse_small_path_length = table2array(PCDGMM_sparse_small(1:size(PCDGMM_sparse_small,1)-2,10));
 
-PCDGMM_sparse_large_path_length = table2array(PCDGMM_sparse_large(1:size(PCDGMM_sparse_large,1)-2,10));
-PCDGMM_sparse_small_path_length = table2array(PCDGMM_sparse_small(1:size(PCDGMM_sparse_small,1)-2,10));
+PCDGMM_clamp_large_path_length = table2array(PCDGMM_clamp_large(1:size(PCDGMM_clamp_large,1)-2,10));
+PCDGMM_clamp_small_path_length = table2array(PCDGMM_clamp_small(1:size(PCDGMM_clamp_small,1)-2,10));
 %%
-ax1=subplot(2,2,1)
-boxplot([PCDEB95_narrow_large_path_length, PCDGMM_narrow_large_path_length],'Labels',{'PCD-EB95','PCD-GMM'});
-title('Narrow environment with large inaccurace')
+% ax1=subplot(3,2,1)
+% boxplot([PCDEB95_narrow_large_path_length, PCDGMM_narrow_large_path_length],'Labels',{'PCD-EB95','PCD-GMM'});
+% title('Narrow environment with large inaccurace')
+% 
+% ax2=subplot(3,2,2)
+% boxplot([PCDEB95_narrow_small_path_length, PCDGMM_narrow_small_path_length],'Labels',{'PCD-EB95','PCD-GMM'});
+% title('Narrow environment with small inaccurace')
+% 
+% 
+% ax3=subplot(3,2,3)
+% boxplot([PCDEB95_sparse_large_path_length, PCDGMM_sparse_large_path_length],'Labels',{'PCD-EB95','PCD-GMM'});
+% title('Sparse environment with large inaccurace')
+% 
+% 
+% ax4=subplot(3,2,4)
+% boxplot([PCDEB95_sparse_small_path_length, PCDGMM_sparse_small_path_length],'Labels',{'PCD-EB95','PCD-GMM'});
+% title('Sparse environment with small inaccurace')
 
-ax2=subplot(2,2,2)
-boxplot([PCDEB95_narrow_small_path_length, PCDGMM_narrow_small_path_length],'Labels',{'PCD-EB95','PCD-GMM'});
-title('Narrow environment with small inaccurace')
+ax5=subplot(3,2,5)
+boxplot([PCDEB95_clamp_large_path_length, PCDGMM_clamp_large_path_length],'Labels',{'PCD-EB95','PCD-GMM'});
+title('Clamp environment with small inaccurace')
 
+ax6=subplot(3,2,6)
+boxplot([PCDEB95_clamp_small_path_length, PCDGMM_clamp_small_path_length],'Labels',{'PCD-EB95','PCD-GMM'});
+title('Clamp environment with small inaccurace')
 
-ax3=subplot(2,2,3)
-boxplot([PCDEB95_sparse_large_path_length, PCDGMM_sparse_large_path_length],'Labels',{'PCD-EB95','PCD-GMM'});
-title('Sparse environment with large inaccurace')
-
-
-ax4=subplot(2,2,4)
-boxplot([PCDEB95_sparse_small_path_length, PCDGMM_sparse_small_path_length],'Labels',{'PCD-EB95','PCD-GMM'});
-title('Sparse environment with small inaccurace')
-
-linkaxes([ax1 ax2 ax3 ax4],'xy')
+linkaxes([ax1 ax2 ax3 ax4 ax5 ax6],'xy')
 
 end
