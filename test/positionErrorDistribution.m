6ce4e1eee74cc260ff0cbfabdbf0842c02ffc450
WANG XIAOLI <71541515+lily983@users.noreply.github.com>
2023-12-15 17:45:52 +0800

    add no position error case

--- test/positionErrorDistribution.m
+++ test/positionErrorDistribution.m
@@ -1,8 +1,8 @@
 function Sigma = positionErrorDistribution(distribution)
 
-if nargin==1
-    distribution='large';
-end
+% if nargin==1
+%     distribution='large';
+% end
 
 Sigma = zeros(3);
 Sigma(1,1) = 7.0000e-00;
@@ -11,15 +11,17 @@ Sigma(3,3) = 9.0000e-00;
 
 switch distribution
     case 'small'
-        sizeScale = 4e-04;
+        scaleValue = 4e-04;
     case 'large'
-        sizeScale = 2e-03;
+        scaleValue = 2e-03;
+    case 'none'
+        scaleValue = 0;
 end
 
 % if strcmp(objectType, 'sphere')
 %     sizeScale = 0.1*sizeScale;
 % end
 
-Sigma = sizeScale.*Sigma;
+Sigma = scaleValue.*Sigma;
 
 end
