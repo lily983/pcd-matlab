f77e96616b976f3c36347b6a545292d36687a2bd
WANG XIAOLI <71541515+lily983@users.noreply.github.com>
2025-11-10 15:22:50 +0800

    rename pcd methods; remove unuseful methods

--- test/add_path.m
+++ test/add_path.m
@@ -1,8 +1,1 @@
-
-addpath ../src/
-addpath ../src/util/
-addpath ../src/geometry
-addpath ../src/positionErrorPCD
-addpath ../src/poseErrorPCD
-addpath ../src/visualizationUtil
-addpath ../test/Violinplot-Matlab
+addpath(genpath('../src/'))
